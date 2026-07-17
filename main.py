599ec50595cd1bbd0912c71d7e9cb2025e7c0ea2
Xc_Star <2035374188@qq.com>
2026-07-17 15:52:45 +0800

    v1.0.5

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
 
-@register("astrbot_plugin_add_one", "Xc_Star", "自动+1", "1.0.4")
+@register("astrbot_plugin_add_one", "Xc_Star", "自动+1", "1.0.5")
 class MyPlugin(Star):
     def __init__(self, context: Context):
-        self.last_msg = ""
+        self.last_msg = {}
         self.is_added = False
         super().__init__(context)
 
@@ -16,14 +16,15 @@ class MyPlugin(Star):
     @filter.event_message_type(filter.EventMessageType.ALL)
     async def add_one(self, event: AstrMessageEvent):
         this_msg = str(event.get_message_str())
-        if this_msg == self.last_msg and not self.is_added:
+        this_group_id = int(event.get_group_id())
+        if this_msg == self.last_msg.get(this_group_id) and not self.is_added:
             self.is_added = True
             yield event.plain_result(this_msg) # 发送一条纯文本消息
 
-        if self.is_added and self.last_msg != this_msg:
+        if self.is_added and self.last_msg.get(this_group_id) != this_msg:
             self.is_added = False
 
-        self.last_msg = this_msg
+        self.last_msg[this_group_id] = this_msg
 
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
