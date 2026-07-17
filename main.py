25b1440b1e0a181a53418d1b4586732434f48591
Xc_Star <2035374188@qq.com>
2026-07-17 15:45:14 +0800

    v1.0.3

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
 
-@register("astrbot_plugin_add_one", "Xc_Star", "自动+1", "1.0.2")
+@register("astrbot_plugin_add_one", "Xc_Star", "自动+1", "1.0.3")
 class MyPlugin(Star):
     def __init__(self, context: Context):
         self.last_msg = ""
@@ -20,8 +20,10 @@ class MyPlugin(Star):
             self.is_added = True
             yield event.plain_result(this_msg) # 发送一条纯文本消息
 
+        if not self.is_added and self.last_msg != this_msg:
+            self.is_added = False
+
         self.last_msg = this_msg
-        self.is_added = False
 
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
