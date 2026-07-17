66bafd3fb917db9166753c964f47bc99677a7d57
Xc_Star <2035374188@qq.com>
2026-07-17 15:37:00 +0800

    v1

--- main.py
+++ main.py
@@ -2,23 +2,25 @@ from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
 
-@register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
+@register("astrbot_plugin_add_one", "Xc_Star", "自动+1", "1.0.0")
 class MyPlugin(Star):
     def __init__(self, context: Context):
+        self.last_msg = ""
+        self.is_added = False
         super().__init__(context)
 
     async def initialize(self):
         """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
 
     # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
-    async def helloworld(self, event: AstrMessageEvent):
-        """这是一个 hello world 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
-        user_name = event.get_sender_name()
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+    @filter.event_message_type(filter.EventMessageType.ALL)
+    async def add_one(self, event: AstrMessageEvent):
+        this_msg = event.get_messages()
+        if this_msg == self.last_msg and not self.is_added:
+            self.is_added = True
+            yield event.plain_result(this_msg) # 发送一条纯文本消息
+
+        self.last_msg = this_msg
 
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
